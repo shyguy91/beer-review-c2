bf3f9a333bf7b9b95c4662861a0b2bcb197de10d
Yeshayahu Wasserman <Shy.Wasserman@gmail.com>
2017-09-03 15:01:22 +0300

    modularized beer app, closure practice

--- script.js
+++ script.js
@@ -1,40 +1,58 @@
-var beers = [];
+function BeerReviewApp() {
+    var beers = [];
 
-function addBeer (name, category, rating) {
-    beers.push({name: name, category: category, rating: rating});
-};
+    function addBeer(name, category, rating) {
+        beers.push({ name: name, category: category, rating: rating });
+    }
 
-$('.post-beer').click(function(){
+    function updateBeer() {
+        var array = $('.beers-list').find('li');
+        for (var i = 0; i < array.length; i++) {
+            array.eq(i).remove();
+        }
+
+        for (var i = 0; i < beers.length; i++) {
+            $('.beers-list').append('<li>' + beers[i].name + ' | ' + beers[i].category + ' | ' + beers[i].rating + '</li>');
+        }
+
+        $('.beers-list').find('li').click(function () {
+            this.remove();
+        });
+    }
+
+    function sortBeer(buttonText){
+        if (buttonText === 'Sort Down') {
+            beers.sort(function (a, b) { return b.rating - a.rating });
+            $('.sort-beer').html('Sort Up');
+        }
+        else if (buttonText === 'Sort Up') {
+            beers.sort(function (a, b) { return a.rating - b.rating });
+            $('.sort-beer').html('Sort Down');
+        }
+        updateBeer();
+    }
+
+    return {
+        addBeer: addBeer,
+        updateBeer: updateBeer,
+        sortBeer: sortBeer
+    };
+}
+
+var app = BeerReviewApp();
+
+$('.post-beer').click(function () {
     var beerInput = $('.beer-input').val();
     var categoryInput = $('.category-input').val();
     var ratingInput = $('.rating-value').val();
-    addBeer(beerInput, categoryInput, ratingInput);
-    updateBeer();
+
+
+    app.addBeer(beerInput, categoryInput, ratingInput);
+    app.updateBeer();
 });
 
-function updateBeer() {
-    var array = $('.beers-list').find('li');
-    for(var i = 0; i < array.length; i++) {
-        array.eq(i).remove();
-    }
 
-    for(var i = 0; i < beers.length; i++) {
-        $('.beers-list').append('<li>' + beers[i].name + ' | ' + beers[i].category + ' | ' + beers[i].rating + '</li>');
-    }
 
-    $('.beers-list').find('li').click(function() {
-        this.remove();
-    });
-};
-
-$('.sort-beer').click(function(){
-    if($('.sort-beer').text() === 'Sort Down') {
-        beers.sort(function(a, b){return b.rating-a.rating});
-        $('.sort-beer').html('Sort Up');
-    } 
-    else if($('.sort-beer').text() === 'Sort Up') {
-        beers.sort(function(a, b){return a.rating-b.rating});
-        $('.sort-beer').html('Sort Down');
-    }   
-    updateBeer();
+$('.sort-beer').click(function () {
+    app.sortBeer($('.sort-beer').text());
 });
